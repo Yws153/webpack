cfca591eea24e3e75e9ea84d2915e5fae34f2453
xfn-yz <yeweishan@dingtalk.com>
2016-11-21 13:29:13 +0800

    修改中

--- webpack.config.js
+++ webpack.config.js
@@ -9,23 +9,20 @@ var config = {
         inline: true,
         port: 2000
     },
+    entry: './src/containers/Note/index.js',
+	output: {
+		path: path.join(__dirname,"./src/containers/out"),
+		filename: "index.js",
+		publicPath: "./src/containers/out"
+	},
     // entry: {
-    //     home: ['./app.js']
-    //     // note: ['./src/containers/Note/index.js']
+    //     // home: ['./src/containers/Home/index.js'],
+    //     note: ['./src/containers/Note/index.js']
     // },
-	// output: {
-	// 	path: path.join(__dirname,"./src/containers/out"),
-	// 	filename: "index.js",
-	// 	publicPath: "./src/containers/out"
-	// },
-    entry: {
-        // home: ['./src/containers/Home/index.js'],
-        note: ['./src/containers/Note/index.js']
-    },
-    output: {
-        path: __dirname,
-        filename: 'build/[name].js'
-    },
+    // output: {
+    //     path: __dirname,
+    //     filename: 'build/[name].js'
+    // },
     resolve: {
         extensions: ['', '.js', '.jsx']
     },
@@ -60,22 +57,22 @@ var config = {
 };
 
 //多文件入口，html模版生成
-for (var name in config.entry) {
-    if (name !== 'lib') {
-        config.plugins.push(new HtmlWebpackPlugin({ //根据模板插入css/js等生成最终HTML
-            // favicon: './src/img/favicon.ico', //favicon路径，通过webpack引入同时可以生成hash值
-            title: name,
-            // chunks: ['lib', name], //需要引入的chunk，不配置就会引入所有页面的资源
-            filename: './app/' + name + '.html', //生成的html存放路径，相对于path
-            template: './src/template/template.html', //html模板路径
-            inject: 'body', //js插入的位置，true/'head'/'body'/false
-            hash: true, //为静态资源生成hash值
-            minify: { //压缩HTML文件
-                removeComments: true, //移除HTML中的注释
-                collapseWhitespace: false //删除空白符与换行符
-            }
-        }));
-    }
-}
+// for (var name in config.entry) {
+//     if (name !== 'lib') {
+//         config.plugins.push(new HtmlWebpackPlugin({ //根据模板插入css/js等生成最终HTML
+//             // favicon: './src/img/favicon.ico', //favicon路径，通过webpack引入同时可以生成hash值
+//             title: name,
+//             // chunks: ['lib', name], //需要引入的chunk，不配置就会引入所有页面的资源
+//             filename: './app/' + name + '.html', //生成的html存放路径，相对于path
+//             template: './src/template/template.html', //html模板路径
+//             inject: 'body', //js插入的位置，true/'head'/'body'/false
+//             hash: true, //为静态资源生成hash值
+//             minify: { //压缩HTML文件
+//                 removeComments: true, //移除HTML中的注释
+//                 collapseWhitespace: false //删除空白符与换行符
+//             }
+//         }));
+//     }
+// }
 
 module.exports = config
